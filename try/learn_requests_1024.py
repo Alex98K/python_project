2208aaa731409bd11afe786b0215325b555e2a58
alex <jiajia172000@163.com>
2020-06-22 08:37:32 +0800

    修改1024文件

--- try/learn_requests_1024.py
+++ try/learn_requests_1024.py
@@ -8,22 +8,21 @@ from concurrent.futures import ThreadPoolExecutor
 # 每个详细页面名字的xpath地址     '//td[contains(@class,'tal')]/h3/a/font/text()'
 # 每个详细页面的xpath地址    '//td[contains(@class,'tal')]/h3/a/@href'
 # 图片xpath地址     '//img/@data-src'
-ALL_TASK = []
 
 
 def download(url):
     try:
-        response = requests.get(url=url, headers=headers, timeout=60)
+        response = requests.get(url=url, headers=headers, timeout=10)
         response.keep_alive = False
         response.encoding = 'gbk'
         if response.status_code != 200:
-            print(url)
+            print(f'这个地址的网页获取失败--{url}')
         return response
     except Exception as e:
         print(e, url)
 
 
-def parse_1(url):
+def page_title_url(url):
     html1 = etree.HTML(res := download(url).text)
     # print(res)
     html_title = html1.xpath("//tr[@class='tr3 t_one tac']/td[contains(@class,'tal')]/h3/a//text()")
@@ -46,7 +45,7 @@ def parse_2(page_adr, thread2):
     def doing():
         if (pic_down_adr := etree.HTML(download(v[1]).text).xpath("//img/@data-src")) and \
                 v[0] != '[岛叔原创]怎么上传图片发布在论坛共享的简单图文教程':
-            # print(pic_down_adr)
+            print(pic_down_adr)
             name = re.sub(rstr, "", v[0])
             dir_path = 'pic/{}'.format(name)
             if not os.path.exists(dir_path):
@@ -58,18 +57,17 @@ def parse_2(page_adr, thread2):
                     for index, url_one in enumerate(pic_down_adr):
                         task = thread2.submit(down_one, url_one, dir_path, name, index)
                         ALL_TASK.append(task)
-                        # print("{}提交一个线程, {}-{}".format(threading.current_thread().name, name, index))
+                        print("{}提交一个线程, {}-{}".format(threading.current_thread().name, name, index))
                         task.running()
                     print(len(ALL_TASK))
     for i, v in enumerate(page_adr):
         doing()
 
 
-def url_head_list(headers):
+def url_head_new(headers):
     url_start = 'https://rr567.net/'
     headers['cookie'] = '__cfduid=d8820278eebccdea98714721a2976fa7b1569319655; _ga=GA1.2.829357220.1569319655; page=http%3A%2F%2Frvedc.com; _gid=GA1.2.334072831.1590379178; fuck1=yes'
-    res = requests.get(url_start, headers=headers).text
-    print(res)
+    res = requests.get(url_start, headers=headers, timeout=3).text
     try:
         head = re.search(r"href='h.*?'", res).group()[6:-1]
         return head
@@ -77,37 +75,47 @@ def url_head_list(headers):
         print('在首页获取1024网址出错')
 
 
-def store_url(url=None):
+def store_return_url(url=None):
     url_temp = None
-    with open('url_head.json', 'a') as f:
-        res_head = json.loads(f)
+    sign = 0
+    with open('url_head.json', 'r') as f:
+        try:
+            head_list = json.load(f)
+        except json.decoder.JSONDecodeError:
+            head_list = []
         if yanzheng_url(url):
             url_temp = url
-        if url not in res_head:
-            if res_head == None:
-                res_head = []
-            res_head.append(url)
-            json.dumps(res_head)
-            print(f"加入新地址--{url}")
+            if url not in head_list:
+                head_list.append(url)
+                sign = 1
         else:
-            for i in res_head:
+            for i in head_list:
                 if yanzheng_url(i):
                     url_temp = i
                     break
+    if sign == 1:
+        with open('url_head.json', 'w') as f:
+            json.dump(head_list, f)
+            print(f"加入新地址--{url}")
     if url_temp:
-        return url_temp
+        return url_temp, head_list
     else:
         print('没有可用的地址')
 
 
 def yanzheng_url(url):
-    res = requests.get(url)
+    try:
+        res = requests.get(url, timeout=3)
+    except requests.exceptions.ReadTimeout:
+        return False
     if res.status_code == 200:
         return True
     else:
         return False
 
+
 if __name__ == '__main__':
+    ALL_TASK = []
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
                  '(KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
     headers = {'user-agent': user_agent}
@@ -116,16 +124,18 @@ if __name__ == '__main__':
     if not os.path.isdir('pic/'):
         os.mkdir('pic/')
     total_pages = 2
-    url_head = url_head_list(headers)
+    url_index, _ = store_return_url(url_head_new(headers))
+    url_head = url_index[:-9]
     print(url_head)
-    # url_list = ['{}/thread0806.php?fid=16&search=&page={}'.format(url_head, i) for i in range(1, total_pages)]
-    # for url in url_list:
-    #     # url = url_list[0]
-    #     page_adr = parse_1(url)
-    #     parse_2(page_adr,  thread2)
-    # # for j in ALL_TASK:
-    #     # print(j.done())
-    # thread2.shutdown(wait=True)
+    url_list = ['{}thread0806.php?fid=16&search=&page={}'.format(url_head, i) for i in range(1, total_pages)]
+    print(url_list)
+    for url in url_list:
+        page_adr = page_title_url(url)
+        # print(page_adr)
+        parse_2(page_adr,  thread2)
+    # for j in ALL_TASK:
+        # print(j.done())
+    thread2.shutdown(wait=True)
 
 
 
