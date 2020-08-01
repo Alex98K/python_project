18eba8be1656b0da01e4b0b96308ab6f6c3b4ca4
alex <jiajia172000@163.com>
2020-08-01 17:14:41 +0800

    学习强国测试版v2.0

--- computer/copy_files/copy_file.py
+++ computer/copy_files/copy_file.py
@@ -3,42 +3,54 @@ import os
 import shutil
 
 
-def get_dir_size(path):  # 计算文件夹大小
-    sum_1 = 0
-    for root1, dirs, files1 in os.walk(path):
-        for file1 in files1:
-            sum_1 += os.stat(os.path.join(root1, file1)).st_size
-    return sum_1
+class CopyFile(object):
+    def __init__(self, path_ore, path_des, exchange=False):
+        if exchange:
+            # 是否要源文件夹和目标文件夹交换再拷贝
+            path_ore, path_des = path_des, path_ore
+        self.path_ore = path_ore
+        self.path_des = path_des
+        self.all_size = 0
+        self.sum_size = 0
+
+    def get_dir_size(self):  # 计算文件夹大小
+        sum_1 = 0
+        for root1, dirs, files1 in os.walk(self.path_ore):
+            for file1 in files1:
+                sum_1 += os.stat(os.path.join(root1, file1)).st_size
+        return sum_1
+
+    def main(self):
+        self.all_size = self.get_dir_size()
+        for root, dir2, files in os.walk(self.path_ore):
+            for dir_o in dir2:
+                ore_dir = os.path.join(root, dir_o)
+                des_dir = ore_dir.replace(self.path_ore, self.path_des)
+                if not os.path.exists(des_dir):
+                    os.makedirs(des_dir)
+            for file in files:
+                ore_file = os.path.join(root, file)
+                des_file = ore_file.replace(self.path_ore, self.path_des)
+                size = os.stat(ore_file).st_size
+                self.sum_size += size
+                process = "\r[共有%.2fGB, 剩余%.2fGB, 完成进度%.5s%%]: |%-50s|" % \
+                          (self.all_size / (1024 * 1024 * 1024),
+                           (self.all_size - self.sum_size) / (1024 * 1024 * 1024),
+                           self.sum_size / self.all_size * 100,
+                           '|' * int(self.sum_size / self.all_size * 50))
+                if not os.path.exists(des_file):
+                    print(process + f'  准备拷贝文件：{ore_file}', end='', flush=True)
+                    shutil.copy2(ore_file, des_file)
+                elif os.stat(ore_file).st_mtime > os.stat(des_file).st_mtime:
+                    print(process + f'  ！覆盖！准备拷贝文件：{ore_file}', end='', flush=True)
+                    shutil.copy2(ore_file, des_file)
+                else:
+                    print(process + f'  跳过这个文件：{ore_file}', end='', flush=True)
+        print('\n', end='\n', flush=True)
+        print('拷贝完成！', end='\n', flush=True)
 
 
-path_ore = r"F:/D/"
-path_des = r"D:/工作/D/"
-
-# 是否要源文件夹和目标文件夹交换再拷贝
-# path_ore, path_des = path_des, path_ore
-
-AllSize = get_dir_size(path_ore)
-size = 0
-sum_a = 0
-for root, dir2, files in os.walk(path_ore):
-    for dir_o in dir2:
-        ore_dir = os.path.join(root, dir_o)
-        des_dir = ore_dir.replace(path_ore, path_des)
-        if not os.path.exists(des_dir):
-            os.makedirs(des_dir)
-    for file in files:
-        ore_file = os.path.join(root, file)
-        des_file = ore_file.replace(path_ore, path_des)
-        size = os.stat(ore_file).st_size
-        sum_a += size
-        process = "\r[共有%.2fGB, 剩余%.2fGB, 完成进度%.5s%%]: |%-50s|" % (AllSize/(1024*1024*1024), (AllSize-sum_a)/(1024*1024*1024), sum_a / AllSize * 100, '|' * int(sum_a / AllSize * 50))
-        if not os.path.exists(des_file):
-            print(process + f'  准备拷贝文件：{ore_file}', end='', flush=True)
-            shutil.copy2(ore_file, des_file)
-        elif os.stat(ore_file).st_mtime > os.stat(des_file).st_mtime:
-            print(process + f'  ！覆盖！准备拷贝文件：{ore_file}', end='', flush=True)
-            shutil.copy2(ore_file, des_file)
-        else:
-            print(process + f'  跳过这个文件：{ore_file}', end='', flush=True)
-print('\n', end='\n', flush=True)
-print('拷贝完成！', end='\n', flush=True)
+if __name__ == '__main__':
+    # do = CopyFile(path_ore=r"F:/D/", path_des=r"D:/工作/D/", exchange=False)
+    do = CopyFile(path_ore=r"F:/D/", path_des=r"D:/工作/D/", exchange=True)
+    do.main()
---
+++ computer/copy_files/progress_main.py
@@ -0,0 +1,61 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QFileDialog
+from PyQt5.QtCore import pyqtSignal, QObject
+from ui import Ui_MainWindow    #上面的界面文件
+from PyQt5.QtGui import QTextCursor
+from copy_file import CopyFile
+
+
+class EmittingStr(QObject):
+    textWritten = pyqtSignal(str)  # 定义一个发送str的信号
+
+    def write(self, text):
+        self.textWritten.emit(str(text))
+
+
+class XXX(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(XXX, self).__init__()
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.path_ore = None
+        self.path_des = None
+        self.pushButton_1.clicked.connect(self.openfile1)
+        self.pushButton_2.clicked.connect(self.openfile2)
+        self.pushButton_3.clicked.connect(self.run)
+
+        # 下面将输出重定向到textBrowser中
+        # sys.stdout = EmittingStr(textWritten=self.output_written)
+        # sys.stderr = EmittingStr(textWritten=self.output_written)
+
+    def run(self):
+        if self.path_ore and self.path_des:
+            do = CopyFile(self.path_ore, self.path_des)
+            do.main()
+            # do.all_size
+            # do.sum_size
+            self.textBrowser_3.setText(do.__repr__())
+
+    def output_written(self, text):
+        cursor = self.textBrowser_3.textCursor()
+        cursor.movePosition(QTextCursor.End)
+        cursor.insertText(text)
+        self.textBrowser_3.setTextCursor(cursor)
+        self.textBrowser_3.ensureCursorVisible()
+
+    def openfile1(self):
+        get_directory_path = QFileDialog.getExistingDirectory(self, "选取指定文件夹", "/")
+        self.textBrowser_1.setText(str(get_directory_path))
+        self.path_ore = str(get_directory_path)
+
+    def openfile2(self):
+        get_directory_path = QFileDialog.getExistingDirectory(self, "选取指定文件夹", "/")
+        self.textBrowser_2.setText(str(get_directory_path))
+        self.path_des = str(get_directory_path)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = XXX()
+    mainWindow.show()
+    sys.exit(app.exec_())
