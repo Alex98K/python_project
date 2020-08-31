4d7b06110055466f8e3c10a5ebbb501ca6cc6629
jiajia172000 <jiajia172000@163.com>
2020-08-31 16:39:22 +0800

    自动阅读项目 V0.1

--- read_phone_project/app_jiao_ben/cai_dan.py
+++ read_phone_project/app_jiao_ben/cai_dan.py
@@ -7,9 +7,12 @@ import time
 class CaiDan(AppReadBase):
     def __init__(self, phone_serial, pp):
         super(CaiDan, self).__init__(phone_serial, pp)
-        # self.pp = uiautomator2.connect_usb()
+        self.pp = uiautomator2.connect_usb()
         self.pp.watcher('tip1').when('我知道了').click()
-        self.pp.watcher('tip2').when('残忍离开').click()
+        self.pp.watcher('tip2').when(xpath='//*[@content-desc="加载中"]/android.view.View[1]/android.view.View[2]/'
+                                           'android.view.View[2]').click()
+        self.pp.watcher('tip3').when(xpath='//*[@resource-id="com.jifen.dandan:id/close_bottom_button"]').click()
+        # self.pp.watcher('tip2').when('残忍离开').click()
         self.pp.watcher.start(0.5)
 
     def log_on(self):
@@ -31,7 +34,9 @@ class CaiDan(AppReadBase):
 
     def sign_in(self):
         self.logger.info(f'开始签到')
-        self.pp(resourceId='com.ss.android.ugc.aweme.lite:id/azz').click(offset=(random.random(), random.random()))
+        self.pp(resourceId='com.jifen.dandan:id/fl_welfare_task').click(offset=(random.random(), random.random()))
+        time.sleep(random.random() + 3)
+        self.pp.xpath('//*[@content-desc="加载中..."]').wait_gone()
         time.sleep(random.random() + 5)
         if self.pp(text='签到').count > 1:
             self.pp(text='签到')[1].click(offset=(random.random(), random.random()))
@@ -44,42 +49,39 @@ class CaiDan(AppReadBase):
         issue_time_start = time.time()  # 开始计时
         while time.time() - issue_time_start <= read_issue_time:
             # 如果不小心切换到了关注栏目，就回到推荐栏目
-            if self.pp.xpath('//*[@resource-id="com.ss.android.ugc.aweme.lite:id/ax_"]').get().center()[0]\
-                    < self.pp(text='关注').bounds()[2]:
-                self.pp(text='推荐').click(offset=(random.random(), random.random()))
+            if self.pp(text='音乐').exists:
+                self.click_random_position(self.pp.xpath('//*[@resource-id="com.jifen.dandan:id/title_container"]/'
+                                                         'android.widget.FrameLayout[3]/android.widget.TextView[1]')
+                                           .get().bounds)
             time.sleep(random.uniform(3, 10))
             # 按照设定的点赞概率，随机点赞
-            if self.pp.xpath('//*[@resource-id="com.ss.android.ugc.aweme.lite:id/ww"]').exists and \
+            if self.pp.xpath('com.jifen.dandan:id/iv_like_icon').exists and \
                     random.random() < self.probability_thumb_up:
-                self.click_random_position(self.pp.xpath('//*[@resource-id="com.ss.android.ugc.aweme.lite:id/ww"]')
-                                           .get().bounds)
+                self.click_random_position(self.pp.xpath('com.jifen.dandan:id/iv_like_icon').get().bounds)
                 time.sleep(random.random() + 1)
             # 按照设定的关注概率，随机关注
-            if self.pp.xpath('//*[@resource-id="com.ss.android.ugc.aweme.lite:id/d14"]').exists and \
+            if self.pp.xpath('//*[@resource-id="com.jifen.dandan:id/fl_follow_view"]').exists and \
                     random.random() < self.probability_focus:
-                self.click_random_position(self.pp.xpath('//*[@resource-id="com.ss.android.ugc.aweme.lite:id/d14"]')
+                self.click_random_position(self.pp.xpath('//*[@resource-id="com.jifen.dandan:id/fl_follow_view"]')
                                            .get().bounds)
-                if self.pp(text='关注').exists(timeout=3):
-                    self.pp(text='关注').click(offset=(random.random(), random.random()))
-                time.sleep(random.random() + 3)
-                self.pp.press('back')
                 time.sleep(random.random() + 1)
             # 按照设定的评论概率，随机评论
-            if self.pp.xpath('//*[@resource-id="com.ss.android.ugc.aweme.lite:id/qb"]').exists and \
+            if self.pp.xpath('com.jifen.dandan:id/iv_comment_icon').exists and \
                     random.random() < self.probability_commit:
-                self.click_random_position(self.pp.xpath('//*[@resource-id="com.ss.android.ugc.aweme.lite:id/qb"]')
-                                           .get().bounds)
+                self.click_random_position(self.pp.xpath('com.jifen.dandan:id/iv_comment_icon').get().bounds)
                 time.sleep(random.random() + 1)
-                if self.pp(text='留下你的精彩评论吧').exists:
-                    self.pp(text='留下你的精彩评论吧').click(offset=(random.random(), random.random()))
-                self.pp(text='留下你的精彩评论吧').wait()
-                self.pp(text='留下你的精彩评论吧').set_text(random.choice(self.commit))
+                if self.pp(resourceId='com.jifen.dandan:id/text_add_comment').exists:
+                    self.pp(resourceId='com.jifen.dandan:id/text_add_comment')\
+                        .click(offset=(random.random(), random.random()))
+                self.pp(resourceId='com.jifen.dandan:id/et_input').wait()
+                self.pp(resourceId='com.jifen.dandan:id/et_input').set_text(random.choice(self.commit))
                 time.sleep(random.random() + 1)
-                self.pp(resourceId='com.ss.android.ugc.aweme.lite:id/qp') \
-                    .click(offset=(random.random(), random.random()))
+                self.pp(resourceId='com.jifen.dandan:id/tv_send').click(offset=(random.random(), random.random()))
                 time.sleep(random.random() + 1)
                 self.pp.press('back')
                 time.sleep(random.random() + 1)
+            if self.pp(description='我的金币').exists:
+                self.pp.press('back')
             self.scroll_read_issue()
 
     def read_issue_first(self):
@@ -87,29 +89,38 @@ class CaiDan(AppReadBase):
         time.sleep(random.random() + 1)
         self.pp(text='首页').click(offset=(random.random(), random.random()))
         time.sleep(random.random() + 1)
+        self.pp(resourceId='com.jifen.dandan:id/lottie_view_gold')\
+            .drag_to(resourceId='com.jifen.dandan:id/iv_ugc_enter', duration=random.uniform(0.25, 0.5))
+        time.sleep(random.random() + 1)
         self._read_issue_core(900, 1200)
 
     def read_issue_city(self):
-        self.logger.info(f'开始阅读同城视频')
-        self.pp(text='同城').click(offset=(random.random(), random.random()))
+        self.logger.info(f'开始阅读发现视频')
+        self.pp(text='发现').click(offset=(random.random(), random.random()))
+        self.pp(resourceId='com.jifen.dandan:id/title_container').wait()
+        self.pp(resourceId='com.jifen.dandan:id/title_container').child()[random.randint(2, 4)]\
+            .click(offset=(random.random(), random.random()))
         for j in range(random.randint(0, 5)):  # 随机下滑几次
             self.pp.swipe(random.uniform(0.3, 0.6), random.uniform(0.7, 0.8), random.uniform(0.3, 0.6),
                           random.uniform(0.2, 0.3), steps=random.randint(20, 60))
         time.sleep(random.random() + 1)
-        temp_bounds = self.pp.xpath(f'//*[@resource-id="com.ss.android.ugc.aweme.lite:id/as7"]/'
-                                    f'android.view.ViewGroup[{random.randint(1, 4)}]/'
-                                    f'android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/'
-                                    f'android.widget.RelativeLayout[1]/android.view.View[1]').bounds
+        temp_bounds = self.pp.xpath(f'//*[@resource-id="com.jifen.dandan:id/taskcenter_rv"]/'
+                                    f'android.widget.LinearLayout[{random.randint(1, 4)}]/'
+                                    f'android.view.ViewGroup[1]').bounds
         self.click_random_position(temp_bounds)  # 随机选页面中的视频
         time.sleep(random.random() + 1)
-        self._read_issue_core(600, 900)
+        self._read_issue_core(60, 90)
+        time.sleep(random.random() + 1)
+        self.pp.press('back')
+        time.sleep(random.random() + 1)
 
     def today_coin(self):
         self.logger.info('获取今日金币数量')
-        self.pp(resourceId='com.ss.android.ugc.aweme.lite:id/azz').click(offset=(random.random(), random.random()))
-        self.pp.xpath('//*[@content-desc="Luckycat"]/android.view.View[2]').wait()
-        coin = self.pp.xpath('//*[@content-desc="Luckycat"]/android.view.View[2]') \
-            .get().attrib['content-desc'].replace(',', '')
+        self.pp(resourceId='com.jifen.dandan:id/fl_welfare_task').click(offset=(random.random(), random.random()))
+        self.pp.xpath('//*[@content-desc="加载中..."]').wait_gone()
+        self.pp.xpath('//*[@resource-id="coins-number"]').wait()
+        time.sleep(random.random() + 2)
+        coin = self.pp.xpath('//*[@resource-id="coins-number"]').get().attrib['content-desc'].replace(',', '')
         time.sleep(random.random() + 1)
         self.pp.press('back')
         time.sleep(random.random() + 1)
@@ -132,8 +143,6 @@ class CaiDan(AppReadBase):
     def clean_cache(self):
         self.logger.info(f'开始清理缓存')
         self.pp(text='我').click(offset=(random.random(), random.random()))
-        self.pp(description='更多').wait()
-        self.pp(description='更多').click(offset=(random.random(), random.random()))
         self.pp(text="设置").wait()
         self.pp(text="设置").click(offset=(random.random(), random.random()))
         t = time.time()
@@ -152,10 +161,10 @@ class CaiDan(AppReadBase):
     def main_do(self):
         # print(self.pp.dump_hierarchy())
         # raise
-        self.app_start('抖音极速版')
+        self.app_start('彩蛋视频')
         self.pp(text='我').wait(timeout=30)
-        self.log_on()
-        self.sign_in()
+        # self.log_on()
+        # self.sign_in()
         self.read_issue()
-        self.clean_cache()
+        # self.clean_cache()
         self.app_end()
